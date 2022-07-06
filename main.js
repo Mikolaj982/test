31583bacb8456a8646727f188412500432e6003a
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2022-07-06 12:59:39 +0200

    fixed mistakes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const convert = () => {
-  const value = document.querySelector('#value');
+  const input = document.querySelector('#input');
   const select = document.querySelector('#select');
   const converted = document.querySelector('#converted');
-  converted.innerHTML === '' ? converted.innerHTML = `TO ${(value.value * select.options[select.selectedIndex].value).toFixed(2)}` : converted.innerHTML = `TO ${(value.value * select.options[select.selectedIndex].value).toFixed(2)}`;
+  converted.innerHTML = `TO ${(input.value * select.options[select.selectedIndex].value).toFixed(2)}`;
 };
 
 (() => { 
   fetch(`https://api.nbp.pl/api/exchangerates/tables/a/today`)
   .then((response) => response.json())
   .then((data) => {
-    console.log( data[0].rates
-      .filter(rate => (rate.code == 'USD' ? USD.value = rate.mid : '' || rate.code == 'EUR' ? EUR.value = rate.mid : '' || rate.code == 'CHF' ? CHF.value = rate.mid : '')))})})();
+    data[0].rates
+      .filter(rate => (rate.code === 'USD' ? USD.value = rate.mid : '' || rate.code === 'EUR' ? EUR.value = rate.mid : '' || rate.code === 'CHF' ? CHF.value = rate.mid : ''))})})();
